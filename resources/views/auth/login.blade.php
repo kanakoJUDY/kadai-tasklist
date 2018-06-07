0524c706219fa08945f2cde9a6fb607a9f22935c
jiro.kirameki <ec2-user@ip-172-31-34-226.us-east-2.compute.internal>
2018-06-07 05:04:49 +0000

    user login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,3 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center">
+        <h1>Log in</h1>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+
+            {!! Form::open(['route' => 'login.post']) !!}
+                <div class="form-group">
+                    {!! Form::label('email', 'Email') !!}
+                    {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                </div>
+
+                <div class="form-group">
+                    {!! Form::label('password', 'Password') !!}
+                    {!! Form::password('password', ['class' => 'form-control']) !!}
+                </div>
+
+                {!! Form::submit('Log in', ['class' => 'btn btn-primary btn-block']) !!}
+            {!! Form::close() !!}
+
+            <p>New user? {!! link_to_route('signup.get', 'Sign up now!') !!}</p>
+        </div>
+    </div>
+@endsection
+Navibar
+Now that you can log in, let’s enrich your navbar. We will also be able to logout.
+
+resources/views/commons/navbar.blade.php
+
 <header>
     <nav class="navbar navbar-inverse navbar-static-top">
         <div class="container">
@@ -8,11 +41,11 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="/">kadai-tasklist</a>
+                <a class="navbar-brand" href="/">Kadai-tasklist</a>
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                  @if (Auth::check())
+                    @if (Auth::check())
                         <li><a href="#">Users</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
